8d5ab07e29b0e0ef701ad0afe84a6ace2ce17d87
Blastdav@gmail.com <blastdav@gmail.com>
2023-11-06 12:47:13 +0000

    Initial script creation

---
+++ scripts/selfControl.py
@@ -0,0 +1,34 @@
+import gradio as gr
+
+from modules import scripts, script_callbacks
+from modules.shared import opts, OptionInfo
+from modules.ui_components import FormRow, FormColumn, FormGroup, ToolButton
+import datetime
+
+NAME = "SD Self Control"
+
+class Script(scripts.Script):
+
+  def title(self):
+    return NAME
+
+
+  def show(self, _is_img2img):
+    return scripts.AlwaysVisible
+
+  def before_component(self, component, **kwargs):
+    # if the current time is in the allowed hours of use, then print "allowed"
+    # else print "not allowed"
+    allowedTimes = opts.Allowed_hours.split(",")
+    if datetime.datetime.now().hour in allowedTimes:
+        print("allowed")
+    else:
+        print("not allowed")
+
+def on_ui_settings():
+
+  section = ("Self control", NAME)
+
+  settings_options = [
+    ("Allowed_hours","17,18,19,20,21,22,23,24","Allowed hours of use CSV of hour in 24 hour format (17,18,19)"),
+  ]
