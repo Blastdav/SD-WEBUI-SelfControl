0bbe90ba720b7856f2508e865dc3ce16c6d1ed01
Blastdav@gmail.com <blastdav@gmail.com>
2023-11-06 18:34:30 +0000

    variable name changing

--- scripts/selfControl.py
+++ scripts/selfControl.py
@@ -8,7 +8,6 @@ import os
 
 NAME = "SD Self Control"
 
-
 class Script(scripts.Script):
 
   def title(self):
@@ -23,31 +22,34 @@ class Script(scripts.Script):
     current_time = str(datetime.datetime.now().hour)
 
     print("Checking if you are allowed to generate")
-    options = {
-               [opts.00option, "0"],
-               [opts.01option, "1"],
-               [opts.02option, "2"],
-               [opts.03option, "3"],
-               [opts.04option, "4"],
-               [opts.05option, "5"],
-               [opts.06option, "6"],
-               [opts.07option, "7"],
-               [opts.08option, "8"],
-               [opts.09option, "9"],
-               [opts.10option, "10"],
-               [opts.11option, "11"],
-               [opts.12option, "12"],
-               [opts.13option, "13"],
-               [opts.14option, "14"],
-               [opts.15option, "15"],
-               [opts.16option, "16"],
-               [opts.17option, "17"],
-               [opts.18option, "18"],
-               [opts.19option, "19"],
-               [opts.20option, "20"],
-               [opts.21option, "21"],
-               [opts.22option, "22"],
-               [opts.23option, "23"]
+
+
+
+    options = {               
+               "0": opts.options00,
+               "1": opts.options01,
+               "2": opts.options02,
+               "3": opts.options03,
+               "4": opts.options04,
+               "5": opts.options05,
+               "6": opts.options06,
+               "7": opts.options07,
+               "8": opts.options08,
+               "9": opts.options09,
+               "10": opts.options10,
+               "11": opts.options11,
+               "12": opts.options12,
+               "13": opts.options13,
+               "14": opts.options14,
+               "15": opts.options15,
+               "16": opts.options16,
+               "17": opts.options17,
+               "18": opts.options18,
+               "19": opts.options19,
+               "20": opts.options20,
+               "21": opts.options21,
+               "22": opts.options22,
+               "23": opts.options23
                }
     
     for option in options:
@@ -72,30 +74,30 @@ def on_ui_settings():
   section = ("Self control", NAME)
 
   settings_options = [
-    ("00option",False,"00:00"),
-    ("01option",False,"01:00"),
-    ("02option",False,"02:00"),
-    ("03option",False,"03:00"),
-    ("04option",False,"04:00"),
-    ("05option",False,"05:00"),
-    ("06option",False,"06:00"),
-    ("07option",False,"07:00"),
-    ("08option",False,"08:00"),
-    ("09option",False,"09:00"),
-    ("10option",False,"10:00"),
-    ("11option",False,"11:00"),
-    ("12option",False,"12:00"),
-    ("13option",False,"13:00"),
-    ("14option",False,"14:00"),
-    ("15option",False,"15:00"),
-    ("16option",False,"16:00"),
-    ("17option",True,"17:00"),
-    ("18option",True,"18:00"),
-    ("19option",True,"19:00"),
-    ("20option",True,"20:00"),
-    ("21option",True,"21:00"),
-    ("22option",True,"22:00"),
-    ("23option",True,"23:00"),
+    ("option00",False,"00:00"),
+    ("option01",False,"01:00"),
+    ("option02",False,"02:00"),
+    ("option03",False,"03:00"),
+    ("option04",False,"04:00"),
+    ("option05",False,"05:00"),
+    ("option06",False,"06:00"),
+    ("option07",False,"07:00"),
+    ("option08",False,"08:00"),
+    ("option09",False,"09:00"),
+    ("option10",False,"10:00"),
+    ("option11",False,"11:00"),
+    ("option12",False,"12:00"),
+    ("option13",False,"13:00"),
+    ("option14",False,"14:00"),
+    ("option15",False,"15:00"),
+    ("option16",False,"16:00"),
+    ("option17",True,"17:00"),
+    ("option18",True,"18:00"),
+    ("option19",True,"19:00"),
+    ("option20",True,"20:00"),
+    ("option21",True,"21:00"),
+    ("option22",True,"22:00"),
+    ("option23",True,"23:00"),
     ("Shutdown_on_disallowed",True,"Shutdown when trying to generate during disallowed hours")
   ]
   
